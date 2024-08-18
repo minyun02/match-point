4e1f2f64c33c3c5850cacfbedf9d2081cee4d079
min <www5981@naver.com>
2024-08-18 21:20:43 +0900

    fix: jwt 유효성 검증 필터, 토큰 생성 클래스를 컴포넌트로 변경

--- src/main/java/com/minsproject/matchpoint/config/filter/JwtTokenFilter.java
+++ src/main/java/com/minsproject/matchpoint/config/filter/JwtTokenFilter.java
@@ -2,33 +2,36 @@ package com.minsproject.matchpoint.config.filter;
 
 import com.minsproject.matchpoint.dto.UserDTO;
 import com.minsproject.matchpoint.service.UserService;
-import com.minsproject.matchpoint.util.JwtTokenUtils;
+import com.minsproject.matchpoint.util.JwtTokenProvider;
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.http.HttpHeaders;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
+import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 
 import java.io.IOException;
 
+import static org.springframework.http.HttpHeaders.AUTHORIZATION;
+
 @Slf4j
+@Component
 @RequiredArgsConstructor
 public class JwtTokenFilter extends OncePerRequestFilter {
 
-    private final String key;
+    private final JwtTokenProvider jwtTokenProvider;
 
     private final UserService userService;
 
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
 
-        final String header = request.getHeader(HttpHeaders.AUTHORIZATION);
+        final String header = request.getHeader(AUTHORIZATION);
 
         if (header == null || !header.startsWith("Bearer=")) {
             log.error("Error occurs while getting header. header is null or invalid {}", request.getRequestURL());
@@ -39,11 +42,11 @@ public class JwtTokenFilter extends OncePerRequestFilter {
         try {
             final String token = header.split(" ")[1].trim();
 
-            if (!JwtTokenUtils.validateToken(token, key)) {
+            if (!jwtTokenProvider.validateToken(token)) {
                 return;
             }
 
-            String email = JwtTokenUtils.getUserEmail(token, key);
+            String email = jwtTokenProvider.getUserEmail(token);
             UserDTO user = userService.loadUserByUserEmail(email);
 
             UsernamePasswordAuthenticationToken authentication = new UsernamePasswordAuthenticationToken(user, null, user.getAuthorities());
--- src/main/java/com/minsproject/matchpoint/util/JwtTokenProvider.java
+++ src/main/java/com/minsproject/matchpoint/util/JwtTokenProvider.java
@@ -4,23 +4,35 @@ import io.jsonwebtoken.*;
 import io.jsonwebtoken.security.Keys;
 import io.jsonwebtoken.security.SecurityException;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.oauth2.client.authentication.OAuth2AuthenticationToken;
+import org.springframework.security.oauth2.core.user.DefaultOAuth2User;
+import org.springframework.stereotype.Component;
 
 import java.nio.charset.StandardCharsets;
 import java.security.Key;
 import java.util.Date;
 
 @Slf4j
-public class JwtTokenUtils {
+@Component
+public class JwtTokenProvider {
+
+    @Value("${jwt.secret-key}")
+    private String key;
+
+    @Value("${jwt.token.expired-time-ms}")
+    private Long expiredTimeMs;
 
     private static final String BEARER_PREFIX = "Bearer= ";
 
-    public static String getUserEmail(String token, String key) {
-        return extractClaims(token, key).get("email", String.class);
+    public String getUserEmail(String token) {
+        return extractClaims(token).get("email", String.class);
     }
 
-    public static boolean validateToken(String token, String key) {
+    public boolean validateToken(String token) {
         try {
-            Jwts.parserBuilder().setSigningKey(getKey(key)).build().parseClaimsJws(token);
+            Jwts.parserBuilder().setSigningKey(getKey()).build().parseClaimsJws(token);
             return true;
         } catch (SecurityException | MalformedJwtException e) {
             log.info("Invalid Token", e);
@@ -34,27 +46,30 @@ public class JwtTokenUtils {
         return false;
     }
 
-    public static Claims extractClaims(String token, String key) {
+    public Claims extractClaims(String token) {
         return Jwts.parserBuilder()
-                .setSigningKey(getKey(key))
+                .setSigningKey(getKey())
                 .build()
                 .parseClaimsJws(token)
                 .getBody();
     }
 
-    public static String generateToken(String email, String key, long expiredTimeMs) {
+    public String generateToken(Authentication authentication) {
+        DefaultOAuth2User oAuth2User = (DefaultOAuth2User) authentication.getPrincipal();
+
         Claims claims = Jwts.claims();
-        claims.put("email", email);
+        claims.put("email", oAuth2User.getAttributes().get("email"));
+        claims.put("provider", ((OAuth2AuthenticationToken) authentication).getAuthorizedClientRegistrationId());
 
         return BEARER_PREFIX + Jwts.builder()
                 .setClaims(claims)
                 .setIssuedAt(new Date(System.currentTimeMillis()))
                 .setExpiration(new Date(System.currentTimeMillis() + expiredTimeMs))
-                .signWith(getKey(key), SignatureAlgorithm.HS256)
+                .signWith(getKey(), SignatureAlgorithm.HS256)
                 .compact();
     }
 
-    private static Key getKey(String key) {
+    private Key getKey() {
         byte[] keyBytes = key.getBytes(StandardCharsets.UTF_8);
         return Keys.hmacShaKeyFor(keyBytes);
     }
