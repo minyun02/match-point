ea65708396a482cfbdeae5bd2364b60d162f0bbf
min <www5981@naver.com>
2024-08-23 08:56:21 +0900

    운동 종목 계정 생성 API에 스웨거 주석 추가

--- src/main/java/com/minsproject/matchpoint/controller/MemberController.java
+++ src/main/java/com/minsproject/matchpoint/controller/MemberController.java
@@ -3,6 +3,8 @@ package com.minsproject.matchpoint.controller;
 import com.minsproject.matchpoint.dto.request.MemberCreateRequest;
 import com.minsproject.matchpoint.dto.response.MemberResponse;
 import com.minsproject.matchpoint.service.MemberService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
@@ -21,6 +23,8 @@ public class MemberController {
     private final MemberService memberService;
 
     @PostMapping
+    @Operation(summary = "사용자의 운동 종목 계정 생성")
+    @ApiResponse(responseCode = "200", description = "만들어진 운동 종목 계정을 반환한다")
     public MemberResponse create(@Valid @RequestBody MemberCreateRequest request) {
         return memberService.create(request);
     }
