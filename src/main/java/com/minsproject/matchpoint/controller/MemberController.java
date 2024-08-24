c6a9b9f17a4a1aa11a6e1fe0ecaa4bb1b80b9ac6
min <www5981@naver.com>
2024-08-24 21:20:03 +0900

    운동 종목 계정 생성 시 닉네임 중복 검사를 위한 api 생성

--- src/main/java/com/minsproject/matchpoint/controller/MemberController.java
+++ src/main/java/com/minsproject/matchpoint/controller/MemberController.java
@@ -8,10 +8,7 @@ import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 
 @Tag(name = "종목별 멤버", description = "사용자의 종목별 멤버 API")
@@ -23,9 +20,16 @@ public class MemberController {
     private final MemberService memberService;
 
     @PostMapping
-    @Operation(summary = "사용자의 운동 종목 계정 생성")
-    @ApiResponse(responseCode = "200", description = "만들어진 운동 종목 계정을 반환한다")
+    @Operation(summary = "사용자의 운동 종목 프로필 생성")
+    @ApiResponse(responseCode = "200", description = "만들어진 운동 종목 프로필을 반환한다")
     public MemberResponse create(@Valid @RequestBody MemberCreateRequest request) {
         return memberService.create(request);
+    }
+
+    @GetMapping("/{nickname}")
+    @Operation(summary = "운동 종목 계정 닉네임으로 조회")
+    @ApiResponse(responseCode = "200", description = "해당 닉네임으로 조회한 결과가 있으면 프로필 정보를 반환하고 없으면 비어있는 프로필을 반환한다")
+    public MemberResponse getMemberByNickname(@PathVariable String nickname) {
+        return memberService.getMemberByNickname(nickname);
     }
 }
