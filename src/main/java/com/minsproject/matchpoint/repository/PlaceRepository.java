a39fd32776588fd31b49f68ea38413baf8e1cfcb
min <www5981@naver.com>
2024-08-28 22:07:47 +0900

    매칭 장소 api 생성

---
+++ src/main/java/com/minsproject/matchpoint/repository/PlaceRepository.java
@@ -0,0 +1,10 @@
+package com.minsproject.matchpoint.repository;
+
+import com.minsproject.matchpoint.entity.Place;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface PlaceRepository extends JpaRepository<Place, Long> {
+
+}
