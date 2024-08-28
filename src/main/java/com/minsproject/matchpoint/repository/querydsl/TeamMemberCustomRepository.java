bce0512b4a8944cf91b0083e5e1c2747f182136a
min <www5981@naver.com>
2024-08-28 22:06:59 +0900

    사용하지 않는 클래스 삭제

--- src/main/java/com/minsproject/matchpoint/repository/querydsl/TeamMemberCustomRepository.java
+++
@@ -1,9 +0,0 @@
-package com.minsproject.matchpoint.repository.querydsl;
-
-import com.minsproject.matchpoint.entity.TeamMember;
-
-import java.util.Optional;
-
-public interface TeamMemberCustomRepository {
-    Optional<TeamMember> findByTeamIdAndUserId(Long teamId, Long userId);
-}
