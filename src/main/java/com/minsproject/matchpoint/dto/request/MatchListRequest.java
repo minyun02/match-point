6bed1dfd966a75f0894d7b2f4fbb27808cec9708
min <www5981@naver.com>
2025-03-04 23:02:25 +0900

    매칭 목록 조회 커맨드 객체에 유효성 검사 애너테이션을 추가

--- src/main/java/com/minsproject/matchpoint/dto/request/MatchListRequest.java
+++ src/main/java/com/minsproject/matchpoint/dto/request/MatchListRequest.java
@@ -1,7 +1,7 @@
 package com.minsproject.matchpoint.dto.request;
 
-
 import com.minsproject.matchpoint.constant.type.SportType;
+import jakarta.validation.constraints.Min;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -9,8 +9,13 @@ import lombok.Setter;
 @Setter
 public class MatchListRequest {
     private Long userId;
+
     private SportType sportType;
+
     private String sort;
+
     private Long lastId;
-    private Integer pageSize;
+
+    @Min(value = 1, message = "페이지 크기는 최소 1 이상이어야 합니다.")
+    private Integer pageSize = 20;
 }
