f5a54d6e0b23115cb3db022e4625a0bff4ec2a5d
min <www5981@naver.com>
2025-03-05 22:19:25 +0900

    매칭 결과 입력 로직 변경

--- src/main/java/com/minsproject/matchpoint/dto/request/MatchResultRequest.java
+++ src/main/java/com/minsproject/matchpoint/dto/request/MatchResultRequest.java
@@ -1,11 +1,13 @@
 package com.minsproject.matchpoint.dto.request;
 
+import jakarta.validation.constraints.Pattern;
 import lombok.Getter;
-import lombok.NoArgsConstructor;
 
 @Getter
-@NoArgsConstructor
 public class MatchResultRequest {
+
+    @Pattern(regexp = "^(win|lose)", message = "유효한 매칭 결과가 아닙니다.")
     private String result;
+
     private Long submitterId;
 }
